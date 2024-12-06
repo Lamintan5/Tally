b08e405617a3e567aa5c22d3defe34658c32a6cd
Lamintan5 <tech@studio5ive.org>
2024-12-06 18:20:55 +0300

    update token

--- users.php
+++ users.php
@@ -10,6 +10,42 @@
     $conn = new mysqli($servername, $username, $password, $dbname);
 
    
+
+
+    if ('UPDATE_TOKEN' == $action) {
+        $uid = $_POST['uid'];
+        $token = $_POST['token'];
+    
+        // Check if the token is empty based on the uid
+        $checkSql = "SELECT token FROM $table WHERE uid = '$uid'";
+        $result = $conn->query($checkSql);
+    
+        if ($result->num_rows > 0) {
+            // $row = $result->fetch_assoc();
+            // if ($row['token']==="") {
+            //     echo "Empty";
+            // } else {
+            //     // If the token is not empty, proceed with the update
+            //     $updateSql = "UPDATE $table SET token = '$token' WHERE uid = '$uid'";
+            //     if ($conn->query($updateSql) === TRUE) {
+            //         echo "success";
+            //     } else {
+            //         echo "error";
+            //     }
+            // }
+            $updateSql = "UPDATE $table SET token = '$token' WHERE uid = '$uid'";
+                if ($conn->query($updateSql) === TRUE) {
+                    echo "success";
+                } else {
+                    echo "error";
+                }
+        } else {
+            echo "Does Not Exist";
+        }
+    
+        $conn->close();
+        return;
+    }
     
 
     if('DELETE' == $action){
