c3d05a8b7a0a31089fe33e333a04f07ac99e7a90
Lamintan5 <tech@studio5ive.org>
2024-12-07 10:08:20 +0300

    update

--- suppliers.php
+++ suppliers.php
@@ -1,5 +1,31 @@
 <?php
     
+    if('UPDATE' == $action){
+        $sid = $_POST['sid'];
+        $name = $_POST['name'];
+        $category = $_POST['category'];
+        $company = $_POST['company'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+
+        $checkSql = "SELECT COUNT(*) as count FROM $table WHERE sid = '$sid'";
+        $result = $conn->query($checkSql);
+        $row = $result->fetch_assoc();
+
+        if ($row['count'] == 0) {
+            echo "Does not exist";
+        } else {
+            $sql = "UPDATE $table SET name = '$name', category = '$category', company = '$company', phone = '$phone', email = '$email' WHERE sid = '$sid'";
+            if ($conn->query($sql) === TRUE) { 
+                echo "success";
+            } else {
+                echo "error";
+            }
+        }
+
+        $conn->close();
+        return;
+    }
 
     if('DELETE_EID' == $action){
         $eid = $_POST['eid'];
