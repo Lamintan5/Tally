8d10a94552ffa16822c67ee02450be80aa086830
Lamintan5 <tech@studio5ive.org>
2024-12-07 10:08:06 +0300

    delete

--- suppliers.php
+++ suppliers.php
@@ -1,3 +1,24 @@
 <?php
     
+    if('DELETE' == $action){
+        $sid = $_POST['sid'];
+        
+        $checkSql = "SELECT COUNT(*) as count FROM $table WHERE sid = '$sid'";
+        $result = $conn->query($checkSql);
+        $row = $result->fetch_assoc();
+
+        if ($row['count'] == 0) {
+            echo "Does not exist";
+        } else {
+            $sql = "DELETE FROM $table WHERE sid = '$sid'";
+            if ($conn->query($sql) === TRUE) {
+                echo "success";
+            } else {
+                echo "error";
+            }
+        }
+        
+        $conn->close();
+        return;
+    }
 ?>
