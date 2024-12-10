8b979cd32267236a5feff0b57021f1e07cd08f48
Lamintan5 <tech@studio5ive.org>
2024-12-10 23:06:14 +0300

    delete

--- duties.php
+++ duties.php
@@ -1,3 +1,14 @@
 <?php
    
+    if('DELETE' == $action){
+        $did = $_POST['did'];
+        $sql = "DELETE FROM $table WHERE did = '$did'";
+        if ($conn->query($sql) === TRUE) {
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
 ?>
