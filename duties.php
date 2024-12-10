6e5c375c82645e24bae14d42d6ca8f86088bf50f
Lamintan5 <tech@studio5ive.org>
2024-12-10 23:06:32 +0300

    update

--- duties.php
+++ duties.php
@@ -1,5 +1,18 @@
 <?php
    
+    if('UPDATE' == $action){
+        $did = $_POST['did'];
+        $duties = $_POST['duties'];  
+        $sql = "UPDATE $table SET  duties = '$duties' WHERE did = '$did'";
+        if ($conn->query($sql) === TRUE) { 
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
+
     if('DELETE_EID' == $action){
         $eid = $_POST['eid'];
         $sql = "DELETE FROM $table WHERE eid = '$eid'";
