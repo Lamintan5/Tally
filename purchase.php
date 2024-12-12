46fa2e2fd8eca64544484a85007cd4687f95a435
Lamintan5 <tech@studio5ive.org>
2024-12-12 20:55:48 +0300

    conn

--- purchase.php
+++ purchase.php
@@ -9,6 +9,10 @@
     $db = mysqli_connect('localhost','root','','tally');
     $conn = new mysqli($servername, $username, $password, $dbname);
 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
 
    
 
