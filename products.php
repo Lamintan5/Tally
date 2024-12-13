a0b02b11647469267bc561c76c3eb58eecf08942
Lamintan5 <tech@studio5ive.org>
2024-12-13 23:42:41 +0300

    delete

--- products.php
+++ products.php
@@ -1,3 +1,15 @@
 <?php
     
+
+    if('DELETE' == $action){
+        $prid = $_POST['prid'];
+        $sql = "DELETE FROM $table WHERE prid = '$prid'";
+        if ($conn->query($sql) === TRUE) {
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
 ?>
