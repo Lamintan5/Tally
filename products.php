07fcad2b825b00e30b0a53e01ce3f81164a05e3f
Lamintan5 <tech@studio5ive.org>
2024-12-13 23:43:43 +0300

    conn

--- products.php
+++ products.php
@@ -1,6 +1,10 @@
 <?php
     
 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
     if('ADD' == $action){
         $prid = $_POST['prid'];
         $eid = $_POST['eid'];
