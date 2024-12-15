712608e3e74e50624e53bd98f644750f49277bab
Lamintan5 <tech@studio5ive.org>
2024-12-15 21:43:12 +0300

    var 1

--- payments.php
+++ payments.php
@@ -1,5 +1,9 @@
 <?php
-    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "tally";
+    $table = "payments";
 
     $action = $_POST['action'];
     $db = mysqli_connect('localhost','root','','tally');
