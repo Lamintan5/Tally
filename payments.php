b6e5d92a3671cd264dc5178fc04c180fa06a9643
Lamintan5 <tech@studio5ive.org>
2024-12-15 21:42:39 +0300

    get by entity

--- payments.php
+++ payments.php
@@ -1,6 +1,21 @@
 <?php
     
 
+    if('GET_BY_ENTITY' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $eid = $_POST['eid'];
+        $query = "SELECT * FROM $table WHERE eid = '".$eid."'";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
+    
+
     if('GET_MY' == $action){
         if ($db->connect_errno) {
             die("Failed to connect to MySQL: " . $db->connect_error);
