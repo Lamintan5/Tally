1717dd862a707be3134af599dbecf388d926abae
Lamintan5 <tech@studio5ive.org>
2024-12-15 21:42:03 +0300

    update

--- payments.php
+++ payments.php
@@ -1,6 +1,27 @@
 <?php
     
 
+    if('UPDATE' == $action){
+        $prid = $_POST['prid'];
+        $name = $_POST['name'];
+        $category = $_POST['category'];
+        $quantity = $_POST['quantity'];
+        $volume = $_POST['volume'];
+        $type = $_POST['type'];
+        $supplier = $_POST['supplier'];
+        $buying = $_POST['buying'];
+        $selling = $_POST['selling'];
+    
+        $sql = "UPDATE $table SET name = '$name', category = '$category', quantity = '$quantity', volume = '$volume',  type = '$type', supplier = '$supplier', buying = '$buying', selling = '$selling' WHERE prid = '$prid'";
+        if ($conn->query($sql) === TRUE) { 
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
+
     if('UPDATE_PURCHASE_AMOUNT' == $action){
         $purchaseid = $_POST['purchaseid'];
         $amount = $_POST['amount'];
