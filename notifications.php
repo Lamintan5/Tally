bed9eb840319a263cf99f68091494d9006c4194f
Lamintan5 <tech@studio5ive.org>
2024-12-16 21:30:05 +0300

    delete

--- notifications.php
+++ notifications.php
@@ -1,3 +1,14 @@
 <?php
    
+    if('DELETE' == $action){
+        $nid = $_POST['nid'];
+        $sql = "DELETE FROM $table WHERE nid = '$nid'";
+        if ($conn->query($sql) === TRUE) {
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
 ?>
