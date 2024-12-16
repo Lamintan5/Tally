00ae91fc8897417a783d20c84983e097e6b1652e
Lamintan5 <tech@studio5ive.org>
2024-12-16 21:30:50 +0300

    get by entity

--- notifications.php
+++ notifications.php
@@ -1,5 +1,18 @@
 <?php
    
+    if('GET_BY_ENTITY' == $action){
+        if ($db->connect_errno) {
+            die("Failed to connect to MySQL: " . $db->connect_error);
+        }
+        $eid = $_POST['eid'];
+        $query = "SELECT * FROM $table WHERE eid = '".$eid."'";
+        $result = $db->query($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
 
     if('UPDATE' == $action){
         $nid = $_POST['nid'];
