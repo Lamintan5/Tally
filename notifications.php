60702b12cbc71e7f74e01de24e0c59515e7ee95a
Lamintan5 <tech@studio5ive.org>
2024-12-16 21:30:23 +0300

    update delete

--- notifications.php
+++ notifications.php
@@ -1,5 +1,42 @@
 <?php
    
+    
+    if ('UPDATE_DELETE' == $action) {
+        $nid = $_POST['nid'];
+        $uid = $_POST['uid'];
+    
+        $sql = "SELECT `deleted` FROM $table WHERE nid = '$nid'";
+        $result = $conn->query($sql);
+        
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $deleteField = $row['deleted'];
+    
+            if (empty($deleteField)) {
+                $newDeleteField = $uid;
+            } else {
+                $uidsArray = explode(',', $deleteField);
+                if (!in_array($uid, $uidsArray)) {
+                    $newDeleteField = $deleteField . ',' . $uid;
+                } else {
+                    $newDeleteField = $deleteField;
+                }
+            }
+
+            $updateSql = "UPDATE $table SET `deleted` = '$newDeleteField' WHERE nid = '$nid'";
+            if ($conn->query($updateSql) === TRUE) {
+                echo "success";
+            } else {
+                echo "failed";
+            }
+        } else {
+            echo "Does not exist";
+        }
+    
+        $conn->close();
+        return;
+    }
+
     if('DELETE_EID' == $action){
         $eid = $_POST['eid'];
         $sql = "DELETE FROM $table WHERE eid = '$eid'";
