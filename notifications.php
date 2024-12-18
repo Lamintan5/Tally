e9b245ef4c90047457fab89c65bf16f3a3475036
Lamintan5 <tech@studio5ive.org>
2024-12-18 23:43:00 +0300

    var 2

--- notifications.php
+++ notifications.php
@@ -5,6 +5,10 @@
     $dbname = "tally";
     $table = "notifications";
 
+    $action = $_POST['action'];
+    $db = mysqli_connect('localhost','root','','tally');
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
     
     
     if('GET_CURRENT' == $action){
