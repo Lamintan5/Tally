769981d91d83638b56c24e01518a927f94c62835
Lamintan5 <tech@studio5ive.org>
2024-12-21 20:38:55 +0300

    delete eid

--- inventory.php
+++ inventory.php
@@ -1,5 +1,17 @@
 <?php
     
+    if('DELETE_EID' == $action){
+        $eid = $_POST['eid'];
+        $sql = "DELETE FROM $table WHERE eid = '$eid'";
+        if ($conn->query($sql) === TRUE) {
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
+
     if('DELETE' == $action){
         $iid = $_POST['iid'];
 
