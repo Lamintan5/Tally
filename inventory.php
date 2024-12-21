fd34d8e3c233bcc982ba446e5f5990f9664eb144
Lamintan5 <tech@studio5ive.org>
2024-12-21 20:39:07 +0300

    update by product id

--- inventory.php
+++ inventory.php
@@ -1,5 +1,18 @@
 <?php
     
+    if('UPDATE_BY_PRODUCTID' == $action){
+        $productid = $_POST['productid'];
+        $quantity = $_POST['quantity'];
+        $sql = "UPDATE $table SET quantity = '$quantity' WHERE productid = '$productid'";
+        if ($conn->query($sql) === TRUE) { 
+            echo "success";
+        } else {
+            echo "failed";
+        }
+        $conn->close();
+        return;
+    }
+
     if('DELETE_EID' == $action){
         $eid = $_POST['eid'];
         $sql = "DELETE FROM $table WHERE eid = '$eid'";
