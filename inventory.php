cdd88aaa059b8c1c7fd76b194de76263fa6ee94f
Lamintan5 <tech@studio5ive.org>
2024-12-21 20:38:44 +0300

    delete

--- inventory.php
+++ inventory.php
@@ -1,3 +1,24 @@
 <?php
     
+    if('DELETE' == $action){
+        $iid = $_POST['iid'];
+
+        $checkSql = "SELECT COUNT(*) as count FROM $table WHERE iid = '$iid'";
+        $result = $conn->query($checkSql);
+        $row = $result->fetch_assoc();
+
+        if ($row['count'] == 0) {
+            echo "Does not exist";
+        } else {
+            $sql = "DELETE FROM $table WHERE iid = '$iid'";
+            if ($conn->query($sql) === TRUE) {
+                echo "success";
+            } else {
+                echo "failed";
+            }
+        }
+        
+        $conn->close();
+        return;
+    }
 ?>
