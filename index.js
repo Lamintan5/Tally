f7ae140a74808d5cb09068c41e7e4f21be97b598
Lamintan5 <tech@studio5ive.org>
2024-12-22 23:25:46 +0300

    messages

--- index.js
+++ index.js
@@ -33,6 +33,54 @@ io.on("connection", (socket) => {
         console.log("Connected clients:", clients);
     });
     
+    socket.on("message", (msg)=>{
+        console.log(msg);
+        let targetId = msg.targetId;
+        let messageText = msg.message;
+        let username = msg.title;
+        let recipientToken = msg.token;
+        let profile = msg.profile;
+        let image = msg.path;
+
+        if (clients[targetId]) {
+            clients[targetId].emit("message", msg);
+            console.log(`Sent message to user ${targetId}`);
+        } else {
+            console.log(`User ${targetId} not found`);
+        }
+        var message = {
+            app_id: ONE_SIGNAL_CONFIG.APP_ID,
+            contents: {en : messageText,},
+            headings: {en: username,},
+            included_segments: ["Subscribed Users"],
+            buttons: [
+                { id: "1", text: "Reply", action: { url: `Tally://message?userId=${targetId}` } },
+                { id: "2", text: "Ignore",},
+            ],
+            include_player_ids: recipientToken,
+            content_available: true,
+            small_icon: "ic_app_log",
+            groupSummaryIcon: "ic_app_log",
+            large_icon: "https://static.wikia.nocookie.net/boondocks/images/a/ac/Cindy_wide_eyes.jpg/revision/latest?cb=20120718190104",
+            // big_picture: "https://10.17.9.72/Tally/profile/riley.jpg",
+            data: {
+                PushTitle: "STUDIO5IVE",
+                group: "123456",
+            },
+            ios_sound: "default", 
+            android_sound: "default",
+            priority: 10,
+        };
+        pushNotificationService.SendNotification(message, (error, results) => {
+            if(error){
+                console.log(`Error`);
+            } else {
+                console.log(`Success`)
+            }
+            
+        });
+    });
+
 
     
 });
