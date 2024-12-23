d99b7af86e645aea8ffc47d72c4251156a04a27a
Lamintan5 <tech@studio5ive.org>
2024-12-23 22:49:33 +0300

    check

--- index.js
+++ index.js
@@ -136,7 +136,9 @@ io.on("connection", (socket) => {
     
 });
 
-
+app.route("/check").get((req, res) => {
+    return res.json("Your app is working fine");
+});
 
 server.listen(port, "0.0.0.0", () => {
     console.log("server started");
